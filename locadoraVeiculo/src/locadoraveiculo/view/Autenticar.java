e83a162dcc25cd70bbc16c2fca756be7015318f9
Eduardo Leite Lapa Canguçu <eduardollcofficial@gmail.com>
2026-08-26 07:30:16 -0300

    Ignorar

--- locadoraVeiculo/src/locadoraveiculo/view/Autenticar.java
+++ locadoraVeiculo/src/locadoraveiculo/view/Autenticar.java
@@ -160,13 +160,13 @@ public class Autenticar extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField2ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
